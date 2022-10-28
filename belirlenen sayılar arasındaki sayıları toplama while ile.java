764288287c8f36b64172b527c8f826432b53ee04
Zerrin <64169761+zerrin07@users.noreply.github.com>
2022-10-28 18:17:45 +0300

    Add files via upload

---
+++ belirlenen sayılar arasındaki sayıları toplama while ile.java
@@ -0,0 +1,25 @@
+package CALİSMA;
+
+public class CALİSMA {
+
+	public static void main(String[] args) {
+		 int sum =0;
+		 
+		 int sayi=10;
+		 
+		while(sayi<=20)
+		{
+		 sum = sum + sayi;
+		 sayi++;
+		}
+		System.out.println(sum);
+	
+		
+		
+		
+		
+		
+
+	}
+
+}
