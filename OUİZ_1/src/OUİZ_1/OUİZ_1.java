df319db517b343e879b58150d8d7fa9b84b976ab
Zerrin <64169761+zerrin07@users.noreply.github.com>
2022-10-28 20:31:37 +0300

    Add files via upload

---
+++ OUİZ_1/src/OUİZ_1/OUİZ_1.java
@@ -0,0 +1,26 @@
+package OUİZ_1;
+
+public class OUİZ_1 {
+
+	public static void main(String[] args) {
+		
+     int x[][]= {{1,4,5},{3,4,5},{7,8,9}};
+     int y[][]= {{3,1,2},{8,4,6},{2,7,8}};
+     int[] v = {4,5,6};
+     
+     for(int i=0;i<3;i++)
+     {
+    	 
+    		 System.out.print(" "+ v[i]); 
+    	 
+     
+    	
+     }
+     
+   
+     
+     
+     
+	}
+
+}
