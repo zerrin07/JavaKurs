7b19344c6c341d5d64c2c6598bf292a7b99a7a32
Zerrin <64169761+zerrin07@users.noreply.github.com>
2022-11-11 10:46:31 +0300

    Add files via upload

---
+++ classes/src/classes/main.java
@@ -0,0 +1,12 @@
+package classes;
+
+public class main {
+
+	public static void main(String[] args) {
+		DortIslem dortIslem = new DortIslem();
+		int sonuc = dortIslem.Topla(3,4);
+		System.out.print(sonuc);
+
+	}
+
+}
