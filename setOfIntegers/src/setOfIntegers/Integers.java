23beca15651108b05220a6952ae8754a58d408df
Zerrin <64169761+zerrin07@users.noreply.github.com>
2022-11-29 19:56:56 +0300

    Add files via upload

---
+++ setOfIntegers/src/setOfIntegers/Integers.java
@@ -0,0 +1,15 @@
+package setOfIntegers;
+
+public class Integers {
+private boolean ArrayList[][] ;
+
+public boolean[][] getArrayList() {
+	return ArrayList;
+}
+
+public void setArrayList(boolean arrayList[][]) {
+	ArrayList = arrayList;
+}
+
+
+}
---
+++ setOfIntegers/src/setOfIntegers/IntegersTest.java
@@ -0,0 +1,10 @@
+package setOfIntegers;
+
+public class IntegersTest {
+
+	public static void main(String[] args) {
+		Integers integerSet = new Integers();
+
+	}
+
+}
