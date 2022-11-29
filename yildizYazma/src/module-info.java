52208eb962b6c8641a18c91a76f44d36463e77b5
Zerrin <64169761+zerrin07@users.noreply.github.com>
2022-11-29 19:50:31 +0300

    Add files via upload

---
+++ yildizYazma/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author ogle
+ *
+ */
+module yildizYazma {
+}
---
+++ yildizYazma/src/yildizYazma/yildizYazma.java
@@ -0,0 +1,26 @@
+package yildizYazma;
+import java.util.Scanner;
+public class yildizYazma {
+   
+	public static void main(String[] args) {
+	
+     int a[][] = {{2,3,4},{5,6,7}};
+     int b[][] = {{3,1,2},{4,3,1}};
+	
+    int[][] c = new int[2][3];
+    for(int i=0;i<a.length;i++)
+    {
+    	for(int j=0;i<a[i].length;j++)
+    	{
+    	c[i][j] = a[i][j] + b[i][j];
+    	System.out.print(c[i][j] + " ");
+    	}
+    	
+		System.out.println();
+    }
+   
+	
+	
+	}
+
+}
