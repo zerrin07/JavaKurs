29299ee32347c52823a79f3ef2731de9930e16bd
Zerrin <64169761+zerrin07@users.noreply.github.com>
2022-12-08 16:44:42 +0300

    Add files via upload

---
+++ ornek/src/ornek/main.java
@@ -0,0 +1,18 @@
+package ornek;
+
+
+
+public class main {
+
+	public static void main(String[] args) {
+	ornek ornek1 = new ornek();
+   
+    System.out.println(ornek1.getSayi());
+    
+    
+
+	
+	
+	}
+
+}
---
+++ ornek/src/ornek/ornek.java
@@ -0,0 +1,20 @@
+package ornek;
+
+public class ornek {
+   private int sayi = 5;
+
+   public ornek() {
+   this.sayi = 6;
+   }
+
+public int getSayi() {
+	return sayi;
+}
+
+public void setSayi(int sayi) {
+	this.sayi = sayi;
+}
+
+
+   
+}
